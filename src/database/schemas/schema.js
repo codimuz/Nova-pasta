1f00ee349cb5128c5947631adc014e1abfba1eef
Seu Nome <seuemail@example.com>
2025-06-03 12:30:54 -0300

    refatora schema do banco de dados para utilizar nova sintaxe e adiciona tabela de entry_changes

--- src/database/schemas/schema.js
+++ src/database/schemas/schema.js
@@ -1,44 +1,58 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const watermelondb_1 = require("@nozbe/watermelondb");
-exports.default = (0, watermelondb_1.appSchema)({
-    version: 1, // Versão do schema
-    tables: [
-        (0, watermelondb_1.tableSchema)({
-            name: 'products',
-            columns: [
-                { name: 'product_code', type: 'string', isIndexed: true },
-                { name: 'product_name', type: 'string', isIndexed: true },
-                { name: 'regular_price', type: 'number' },
-                { name: 'club_price', type: 'number' },
-                { name: 'unit_type', type: 'string' },
-                { name: 'created_at', type: 'number' },
-                { name: 'updated_at', type: 'number' },
-                { name: 'deleted_at', type: 'number', isOptional: true },
-                { name: 'restored_at', type: 'number', isOptional: true },
-            ],
-        }),
-        (0, watermelondb_1.tableSchema)({
-            name: 'reasons',
-            columns: [
-                { name: 'code', type: 'string', isIndexed: true },
-                { name: 'description', type: 'string' },
-                { name: 'created_at', type: 'number' },
-                { name: 'updated_at', type: 'number' },
-            ],
-        }),
-        (0, watermelondb_1.tableSchema)({
-            name: 'entries',
-            columns: [
-                { name: 'product_code_value', type: 'string', isIndexed: true },
-                { name: 'product_name', type: 'string' },
-                { name: 'quantity', type: 'number' },
-                { name: 'reason_code_value', type: 'string', isIndexed: true },
-                { name: 'entry_date', type: 'number', isIndexed: true },
-                { name: 'is_synchronized', type: 'boolean', isOptional: true },
-                { name: 'linked_product_id', type: 'string', isIndexed: true },
-                { name: 'linked_reason_id', type: 'string', isIndexed: true },
-            ],
-        }),
-    ],
+import { appSchema, tableSchema } from '@nozbe/watermelondb';
+
+export default appSchema({
+  version: 1, // Versão do schema
+  tables: [
+    tableSchema({
+      name: 'products',
+      columns: [
+        { name: 'product_code', type: 'string', isIndexed: true },
+        { name: 'product_name', type: 'string', isIndexed: true },
+        { name: 'regular_price', type: 'number' },
+        { name: 'club_price', type: 'number' },
+        { name: 'unit_type', type: 'string' },
+        { name: 'created_at', type: 'number' },
+        { name: 'updated_at', type: 'number' },
+        { name: 'deleted_at', type: 'number', isOptional: true },
+        { name: 'restored_at', type: 'number', isOptional: true },
+      ],
+    }),
+    tableSchema({
+      name: 'reasons',
+      columns: [
+        { name: 'code', type: 'string', isIndexed: true },
+        { name: 'description', type: 'string' },
+        { name: 'created_at', type: 'number' },
+        { name: 'updated_at', type: 'number' },
+      ],
+    }),
+    tableSchema({
+      name: 'entries',
+      columns: [
+        { name: 'product_code_value', type: 'string', isIndexed: true },
+        { name: 'product_name', type: 'string' },
+        { name: 'quantity', type: 'number' },
+        { name: 'reason_code_value', type: 'string', isIndexed: true },
+        { name: 'entry_date', type: 'number', isIndexed: true },
+        { name: 'is_synchronized', type: 'boolean', isOptional: true },
+        { name: 'linked_product_id', type: 'string', isIndexed: true },
+        { name: 'linked_reason_id', type: 'string', isIndexed: true },
+      ],
+    }),
+    tableSchema({
+      name: 'entry_changes',
+      columns: [
+        { name: 'product_code', type: 'string', isIndexed: true },
+        { name: 'product_name', type: 'string' },
+        { name: 'old_quantity', type: 'number', isOptional: true },
+        { name: 'new_quantity', type: 'number' },
+        { name: 'old_reason_id', type: 'string', isOptional: true, isIndexed: true },
+        { name: 'new_reason_id', type: 'string', isIndexed: true },
+        { name: 'change_date', type: 'number', isIndexed: true },
+        { name: 'action_type', type: 'string' }, // 'insertion', 'edition', 'removal', 'movement'
+        { name: 'created_at', type: 'number' },
+        { name: 'updated_at', type: 'number' },
+      ],
+    }),
+  ],
 });
